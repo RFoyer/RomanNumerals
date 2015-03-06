2edc9fa0ac6554ff19f5d39b3e063e74f91b7b05
RFoyer <paulinist@hotmail.com>
2015-03-05 21:30:53 -0600

    Four Different Choices

---
+++ RomanNumeral/Encoder.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeral
+{
+    public class Encoder
+    {
+
+        // Choice 1: normal (easiest) way. 
+
+        Dictionary<int, string> romanKey = new Dictionary<int, string>() {
+            { 1000, "M" },
+            { 900, "CM" },
+            { 500, "D" },
+            { 400, "CD" },
+            { 100, "C" },
+            { 90, "XC" },
+            { 50, "L" },
+            { 40, "XL" },
+            { 10, "X" },
+            { 9, "IX" },
+            { 5, "V" },
+            { 4, "IV" },
+            { 1, "I" }
+        };
+
+        public string ToRoman(int number)
+        {
+
+            string romanNumeral = "";
+
+            while (number >= 1)
+            {
+                foreach (KeyValuePair<int, string> kvp in romanKey)
+                {
+                    if (number >= kvp.Key)
+                    {
+                        romanNumeral += kvp.Value;
+                        number -= kvp.Key;
+                        break;
+                    }
+                }
+            }
+
+            return romanNumeral;
+        }
+
+        // Choice 2 (my idea): build numeral one digit at a time from right to left, use Insert method.
+
+        //string[] tallyNumerals = { "I", "X", "C", "M" };
+        //string[] fiveNumerals = { "V", "L", "D" };
+
+        //public string ToRoman(int number)
+        //{
+
+        //    string numberAsRoman = "";
+        //    string numberAsString = number.ToString();
+
+        //    for (int i = 0; i < numberAsString.Length; i++)
+        //    {
+        //        int currentDigitAsInt = Int32.Parse(numberAsString[numberAsString.Length - 1 - i].ToString());
+        //        string currentDigitAsRoman = "";
+
+        //        if (currentDigitAsInt == 9)
+        //        {
+        //            currentDigitAsRoman += tallyNumerals[i] + tallyNumerals[i + 1];
+        //            currentDigitAsInt -= 9;
+        //        }
+
+        //        if (currentDigitAsInt >= 5)
+        //        {
+        //            currentDigitAsRoman += fiveNumerals[i];
+        //            currentDigitAsInt -= 5;
+        //        }
+
+        //        if (currentDigitAsInt == 4)
+        //        {
+        //            currentDigitAsRoman += tallyNumerals[i] + fiveNumerals[i];
+        //        }
+
+        //        if (currentDigitAsInt <= 3)
+        //        {
+        //            for (int j = 0; j < currentDigitAsInt; j++)
+        //            {
+        //                currentDigitAsRoman += tallyNumerals[i];
+        //            }
+        //        }
+
+        //        numberAsRoman = numberAsRoman.Insert(0, currentDigitAsRoman);
+
+        //    }
+
+        //    return numberAsRoman;
+        //}
+
+        // Choice 3 (my idea): build numeral from left to right, but have to use indexConverter to find starting index on the string arrays.
+
+        //string[] tallyNumerals = { "M", "C", "X", "I" };
+        //string[] fiveNumerals = { "", "D", "L", "V" };
+        //int[] indexConverter = { 3, 2, 1, 0 };
+
+        //public string ToRoman(int number)
+        //{
+
+        //    string numberAsRoman = "";
+        //    string numberAsString = number.ToString();
+        //    int startingIndex = indexConverter[numberAsString.Length - 1];
+
+        //    for (int i = 0; i < numberAsString.Length; i++)
+        //    {
+        //        int currentDigitAsInt = Int32.Parse(numberAsString[0 + i].ToString());
+        //        string currentDigitAsRoman = "";
+
+        //        if (currentDigitAsInt == 9)
+        //        {
+        //            currentDigitAsRoman += tallyNumerals[startingIndex + i] + tallyNumerals[startingIndex + i - 1];
+        //            currentDigitAsInt -= 9;
+        //        }
+
+        //        if (currentDigitAsInt >= 5)
+        //        {
+        //            currentDigitAsRoman += fiveNumerals[startingIndex + i];
+        //            currentDigitAsInt -= 5;
+        //        }
+
+        //        if (currentDigitAsInt == 4)
+        //        {
+        //            currentDigitAsRoman += tallyNumerals[startingIndex + i] + fiveNumerals[startingIndex + i];
+        //        }
+
+        //        if (currentDigitAsInt <= 3)
+        //        {
+        //            for (int j = 0; j < currentDigitAsInt; j++)
+        //            {
+        //                currentDigitAsRoman += tallyNumerals[startingIndex + i];
+        //            }
+        //        }
+
+        //        numberAsRoman += currentDigitAsRoman;
+
+        //    }
+
+        //    return numberAsRoman;
+        //}
+
+        // Choice 4: Mike Harris' REPL one-liner.
+
+        //public Func<int, string> ToRoman = (number) =>
+        //    new Dictionary<int, string> {
+        //    { 1000, "M" },
+        //    { 900, "CM" },
+        //    { 500, "D" },
+        //    { 400, "CD" },
+        //    { 100, "C" },
+        //    { 90, "XC" },
+        //    { 50, "L" },
+        //    { 40, "XL" },
+        //    { 10, "X" },
+        //    { 9, "IX" },
+        //    { 5, "V" },
+        //    { 4, "IV" },
+        //    { 1, "I" } //this last dictionary element is not actually necessary
+        //}.Aggregate(new string('I', number), (m, _) => m.Replace(new string('I', _.Key), _.Value));
+
+    }
+}
---
+++ RomanNumeralTests/EncoderTests.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanNumeral;
+
+namespace RomanNumeralTests
+{
+    [TestClass]
+    public class EncoderTests
+    {
+        [TestMethod]
+        public void Encoder_1_Returns_I()
+        {
+            var encoder = new Encoder();
+            var number = 1;
+
+            var actual = encoder.ToRoman(number);
+
+            Assert.AreEqual("I", actual);
+        }
+
+        [TestMethod]
+        public void Encoder_2_Returns_II()
+        {
+            var encoder = new Encoder();
+            var number = 2;
+
+            var actual = encoder.ToRoman(number);
+
+            Assert.AreEqual("II", actual);
+        }
+
+        [TestMethod]
+        public void Encoder_4_Returns_IV()
+        {
+            var encoder = new Encoder();
+            var number = 4;
+
+            var actual = encoder.ToRoman(number);
+
+            Assert.AreEqual("IV", actual);
+        }
+
+        [TestMethod]
+        public void Encoder_5_Returns_V()
+        {
+            var encoder = new Encoder();
+            var number = 5;
+
+            var actual = encoder.ToRoman(number);
+
+            Assert.AreEqual("V", actual);
+        }
+
+        [TestMethod]
+        public void Encoder_8_Returns_VIII()
+        {
+            var encoder = new Encoder();
+            var number = 8;
+
+            var actual = encoder.ToRoman(number);
+
+            Assert.AreEqual("VIII", actual);
+        }
+
+        [TestMethod]
+        public void Encoder_9_Returns_IX()
+        {
+            var encoder = new Encoder();
+            var number = 9;
+
+            var actual = encoder.ToRoman(number);
+
+            Assert.AreEqual("IX", actual);
+        }
+
+        [TestMethod]
+        public void Encoder_10_Returns_X()
+        {
+            var encoder = new Encoder();
+            var number = 10;
+
+            var actual = encoder.ToRoman(number);
+
+            Assert.AreEqual("X", actual);
+        }
+
+        [TestMethod]
+        public void Encoder_14_Returns_XIV()
+        {
+            var encoder = new Encoder();
+            var number = 14;
+
+            var actual = encoder.ToRoman(number);
+
+            Assert.AreEqual("XIV", actual);
+        }
+
+        [TestMethod]
+        public void Encoder_18_Returns_XVIII()
+        {
+            var encoder = new Encoder();
+            var number = 18;
+
+            var actual = encoder.ToRoman(number);
+
+            Assert.AreEqual("XVIII", actual);
+        }
+
+        [TestMethod]
+        public void Encoder_19_Returns_XIX()
+        {
+            var encoder = new Encoder();
+            var number = 19;
+
+            var actual = encoder.ToRoman(number);
+
+            Assert.AreEqual("XIX", actual);
+        }
+
+        [TestMethod]
+        public void Encoder_69_Returns_LXIX()
+        {
+            var encoder = new Encoder();
+            var number = 69;
+
+            var actual = encoder.ToRoman(number);
+
+            Assert.AreEqual("LXIX", actual);
+        }
+
+        [TestMethod]
+        public void Encoder_99_Returns_XCIX()
+        {
+            var encoder = new Encoder();
+            var number = 99;
+
+            var actual = encoder.ToRoman(number);
+
+            Assert.AreEqual("XCIX", actual);
+        }
+
+        [TestMethod]
+        public void Encoder_100_Returns_C()
+        {
+            var encoder = new Encoder();
+            var number = 100;
+
+            var actual = encoder.ToRoman(number);
+
+            Assert.AreEqual("C", actual);
+        }
+
+        [TestMethod]
+        public void Encoder_455_Returns_CDLV()
+        {
+            var encoder = new Encoder();
+            var number = 455;
+
+            var actual = encoder.ToRoman(number);
+
+            Assert.AreEqual("CDLV", actual);
+        }
+
+        [TestMethod]
+        public void Encoder_999_Returns_CMXCIX()
+        {
+            var encoder = new Encoder();
+            var number = 999;
+
+            var actual = encoder.ToRoman(number);
+
+            Assert.AreEqual("CMXCIX", actual);
+        }
+
+        [TestMethod]
+        public void Encoder_1234_Returns_MCCXXXIV()
+        {
+            var encoder = new Encoder();
+            var number = 1234;
+
+            var actual = encoder.ToRoman(number);
+
+            Assert.AreEqual("MCCXXXIV", actual);
+        }
+
+        [TestMethod]
+        public void Encoder_3999_Returns_MMMCMXCIX()
+        {
+            var encoder = new Encoder();
+            var number = 3999;
+
+            var actual = encoder.ToRoman(number);
+
+            Assert.AreEqual("MMMCMXCIX", actual);
+        }
+
+    }
+}
